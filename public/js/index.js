f249aaec37c84687d0723080517eaf5085961fe5
Richard Turner <richardturnerdeveloper@gmail.com>
2017-09-20 15:06:16 -0400

    Scroll if user is scrolled to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom (){
+  var messages = $('#messages');
+  var newMessage= messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('CONNECTED TO SERVER!');
 
@@ -19,7 +34,7 @@ socket.on('newMessage', function (message){
   });
 
   $('#messages').append(html);
-
+  scrollToBottom();
 
   // var li = $('<li></li>');
   // li.text(`${message.from}: ${formattedTime} ${message.text}`);
@@ -38,6 +53,7 @@ socket.on('newLocationMessage', function(message){
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 
   // li.text(`${message.from}: ${formattedTime} `);
   // a.attr('href', message.url)
